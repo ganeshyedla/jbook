9f6552623d4781671eb128f48e936430c789298a
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2023-02-14 08:27:27 +0000

    monaco editor added

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,36 @@
+import MonacoEditor, {EditorDidMount} from "@monaco-editor/react";
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount: EditorDidMount = (getValue,monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue())
+    });
+  };
+  
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+      theme="dark"
+      language="javascript"
+      height="500px"
+    />
+  );
+};
+
+export default CodeEditor;
