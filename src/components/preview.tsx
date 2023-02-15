eab56a1c918ebeae24fda609793c1fba60abee5d
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2023-02-15 09:47:00 +0000

    better error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 
 interface PreviewProps {
   code: string;
+  err: string;
 }
 
 const html = `
@@ -11,13 +12,22 @@ const html = `
   <body>
     <div id="root"></div>
     <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+        console.error(err);
+      };
+
+      window.addEventListener('error', (event) => {
+        event.preventDefault();
+        handleError(event.error);
+      }
+      );
       window.addEventListener('message', (event) => {
         try {
           eval(event.data);
         } catch (err) {
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-          console.error(err);
+          handleError(err);
         }
       }, false);
     </script>
@@ -25,12 +35,12 @@ const html = `
 </html>
 `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    setTimeout(() => {      
+    setTimeout(() => {
       iframe.current.contentWindow.postMessage(code, "*");
     }, 50)
   }, [code]);
@@ -39,7 +49,13 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   return (
     <div className="preview-wrapper">
       <iframe
+        title='preview-pane'
         ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+        {err && 
+        <div className="preview-error">
+          {err}
+        </div>
+        }
     </div>
   )
 };
