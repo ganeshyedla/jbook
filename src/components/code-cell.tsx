dbf868326c4cd4c1c80e75a4106e73d90cc2ff8e
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2023-02-15 09:17:06 +0000

    added debouncing in code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
 import bundle from "../bundler";
@@ -8,19 +8,26 @@ const CodeCell = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input)
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input)
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [input])
 
   return (
     <Resizable direction="vertical">
-      <div style={{height: '100%', display:'flex', flexDirection: 'row'}}>
-        <CodeEditor
-          initialValue="const a = 1;"
-          onChange={(value) => setInput(value)}
-        />
-        {/* <pre>{code}</pre> */}
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
