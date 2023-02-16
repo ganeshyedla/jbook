7a8f32e235e99a462b911cf7a2eb87cae2068f0e
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2023-02-16 14:09:04 +0000

    added redux scaffolding

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import ReactDOM from "react-dom/client";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 // import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 const App = () => {
 
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
