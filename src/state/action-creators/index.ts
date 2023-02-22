fb4ef88217185f2cc376f19755df79ee722f0da5
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2023-02-22 14:21:58 +0000

    added bundles reducer

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,9 @@
+import { builtinModules } from "module";
+import { Dispatch } from "redux";
+import bundle from "../../bundler";
 import { ActionType } from "../action-types";
 import {
+  Action,
   DeleteCellAction,
   Direction,
   InsertCellAfterAction,
@@ -50,3 +54,24 @@ export const insertCellAfter = (
     },
   };
 };
+
+export const createBundle = (id: string, input: string) => {
+  return async(dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        id
+      }
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        id,
+        bundle: result
+      }
+    })
+  };
+}
