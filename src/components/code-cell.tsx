ad10e89a5d13e45b6e0405e7974386b3b0106ac6
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2023-02-23 07:03:54 +0000

    cumulative execution - WIP

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,16 +5,22 @@ import Resizable from "./resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import "./code-cell.css";
 
 interface CodeCellProps {
   cell: Cell
 }
 
-const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
-  const {updateCell, createBundle} = useActions();
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
@@ -30,10 +36,19 @@ const CodeCell: React.FC<CodeCellProps> = ({cell}) => {
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue={cell.content}
-            onChange={(value) => updateCell(cell.id,value)}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        { bundle &&  <Preview code={bundle.code} err={bundle.err}/>}
+        <div className="progress-wrapper">
+          {
+            !bundle || bundle.loading ?
+              <div className="progress-cover">
+                <progress className="progress is-small is-primary" max="100">
+                  Loading
+                </progress>
+              </div> : <Preview code={bundle.code} err={bundle.err} />
+          }
+        </div>
       </div>
     </Resizable>
   );
